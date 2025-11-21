0fb61eb1aea044649c9ee9dd5134233f6f437caa
glitterfart9000 <jacobcampcs@gmail.com>
2025-11-21 17:44:25 +0000

    worked on local storage and id system, debugged

--- script.js
+++ script.js
@@ -35,46 +35,12 @@ function addExpense() {
     let idNum = generateIdNum();
 
     createExpenseRow(userAmount, userCategory, userDesc, userDate, idNum);
-}
-
-function createExpenseRow(userAmount, userCategory, userDesc, userDate, idNum) {
-    // add new row
-    let newExpenseRow = document.createElement("tr");
-    newExpenseRow.id = "row" + idNum;
-    let tableBody = document.getElementById("expensesList");
-
-    // add table data for 'date'
-    let dateData = document.createElement("td");
-    dateData.innerHTML = userDate;
-    newExpenseRow.appendChild(dateData);
-
-    // add table data for 'category'
-    let categoryData = document.createElement("td");
-    categoryData.innerHTML = userCategory;
-    newExpenseRow.appendChild(categoryData);
-
-    // add table data for 'description'
-    let descData = document.createElement("td");
-    descData.innerHTML = userDesc;
-    newExpenseRow.appendChild(descData);
-
-    // add table data for 'amount'
-    let expenseAmtData = document.createElement("td");
-    expenseAmtData.innerHTML = userAmount;
-    newExpenseRow.appendChild(expenseAmtData);
-
-    // add delete button
-    let deleteButton = document.createElement("button");
-    deleteButton.addEventListener("click", removeTask)
-    deleteButton.classList.add("button");
-    deleteButton.innerText = "Delete";
-    newExpenseRow.appendChild(deleteButton);
-
-    // add to table
-    tableBody.appendChild(newExpenseRow);
 
     // save to local storage
-    localStorage.setItem("row" + idNum, newExpenseRow);
+    localStorage.setItem("exp" + idNum, userAmount);
+    localStorage.setItem("cat" + idNum, userCategory);
+    localStorage.setItem("des" + idNum, userDesc);
+    localStorage.setItem("dat" + idNum, userDate);
 }
 
 function generateIdNum() {
@@ -87,10 +53,52 @@ function generateIdNum() {
     return idNum;
 }
 
+function createExpenseRow(userAmount, userCategory, userDesc, userDate, idNum) {
+    // add new row
+    let newExpenseRow = document.createElement("tr");
+    newExpenseRow.id = "row" + idNum;
+    let tableBody = document.getElementById("expensesList");
 
-function removeTask(event) {
+    // add table data for 'date'
+    let dateData = document.createElement("td");
+    dateData.id = "dat" + idNum
+    dateData.innerText = userDate;
+    newExpenseRow.appendChild(dateData);
+
+    // add table data for 'category'
+    let categoryData = document.createElement("td");
+    categoryData.id = "cat" + idNum;
+    categoryData.innerText = userCategory;
+    newExpenseRow.appendChild(categoryData);
+
+    // add table data for 'description'
+    let descData = document.createElement("td");
+    descData.id = "des" + idNum;
+    descData.innerText = userDesc;
+    newExpenseRow.appendChild(descData);
+
+    // add table data for 'amount'
+    let expenseAmtData = document.createElement("td");
+    expenseAmtData.id = "exp" + idNum;
+    expenseAmtData.innerText = userAmount;
+    newExpenseRow.appendChild(expenseAmtData);
+
+    // add delete button
+    let deleteButton = document.createElement("button");
+    deleteButton.addEventListener("click", removeTask)
+    deleteButton.classList.add("button");
+    deleteButton.innerText = "Delete";
+    newExpenseRow.appendChild(deleteButton);
+
+    // add to table
+    tableBody.appendChild(newExpenseRow);
+
+}
+
+
+function removeRow(event) {
     // get id of button
-    let expenseId = event.target.id; // e.g. checkbox10
+    let expenseId = event.target.id;
 
     // get id number from button id
     let idNum = expenseId.substring(8);
@@ -102,14 +110,20 @@ function removeTask(event) {
     expenseRow.remove();
 }
 
-function loadTasks() {
+function loadRows() {
     // get each task from local storage
     for (let i = 0; i < localStorage.length; i++){
-        let key = localStorage.key(i);
-        let expense = localStorage.getItem(key);
+        let key0 = localStorage.key(i);
+        let key1 = localStorage.key(i+1);
+        let key2 = localStorage.key(i+2);
+        let key3 = localStorage.key(i+3);
+        let amount = localStorage.getItem(key0);
+        let category = localStorage.getItem(key1);
+        let desc = localStorage.getItem(key2);
+        let date = localStorage.getItem(key3);
         // create task divs for each task
-        addExpense(expense, key.substring(3));
+        createExpenseRow(amount, category, desc, date, key0.substring(3));
     }
 }
 
-loadTasks();
+loadRows();
